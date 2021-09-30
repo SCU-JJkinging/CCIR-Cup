9cf5711c5328eb1c795d85231ee7f2195a491fb4
jinxiang <2452393862@qq.com>
2021-09-30 15:15:35 +0800

    update

--- image/run_infer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /ccir/data/code/predict
-python run_trained_JointBert.py
-python run_trained_interact1.py
-python run_trained_interact3.py
-python integration.py
-echo '模型已全部推理完成，结果result.json已保存在prediction_result文件夹下'
-
